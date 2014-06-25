1799861b162e50059f9b9656d3e1e58c40915f2e
thomaslam <tlaminator@Thomass-MacBook-Air.local>
2014-06-25 13:31:03 -0700

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Source/Grid.m
+++ GameOfLife.spritebuilder/Source/Grid.m
@@ -137,7 +137,7 @@ static const int GRID_COLUMNS = 10;
 - (void) updateCreatures {
     int numAlive = 0;
     for (int i = 0; i < [_gridArray count]; i++) {
-        for (int j=0; j < [_gridArray count]; j++) {
+        for (int j=0; j < [_gridArray[i] count]; j++) {
             Creature *currentCreature = _gridArray[i][j];
             if (currentCreature.livingNeighbors == 3) {
                 currentCreature.isAlive = YES;
